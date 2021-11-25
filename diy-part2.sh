08f2289b995b78488fedc740c205ba82e12e59bd
neabei <67257687+neabei@users.noreply.github.com>
2021-11-25 14:55:16 +0800

    Update diy-part2.sh
    修改主机名

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把Xiaoyu修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/Xiaoyu/g' package/base-files/files/bin/config_generate
